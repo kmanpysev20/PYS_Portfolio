25f3e48c9062bc51baf9ba4e8446ca192ff81020
kmanpysev20 <kmanpysev20@gmail.com>
2024-04-08 15:24:18 +0900

    v0.17

--- js/intro.js
+++ js/intro.js
@@ -1,3 +1,93 @@
+// intro_scrollTrigger [s]
+$(function () {
+  $(".visual .tit").fadeIn(2000);
+
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollSmoother);
+
+  // 01.visual
+  const visual = gsap.timeline();
+  visual.to(
+    ".visual img",
+    { scale: 0.4, ease: "none", duration: "10", opactity: "0.3" },
+    1
+  );
+
+  ScrollTrigger.create({
+    animation: visual,
+    trigger: ".visual",
+    start: "top top",
+    end: "bottom top",
+    scrub: 1,
+  });
+
+  // gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ".visual",
+  //       start: "top top",
+  //       end: "bottom top",
+  //       scrub: 1,
+  //       // markers: true,
+  //     },
+  //   })
+  // .to(".visual img", { scale: 0.4, ease: 'none' , duration: "10", opactity : '0.3' },1);
+
+  // 02. imgBox
+  gsap.utils.toArray(".imgBox").forEach(function (imgBox) {
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: imgBox,
+        start: "50% 100%",
+        toggleClass: { targets: imgBox, className: "active" },
+        scrub: 1,
+        // markers: true,
+      },
+    });
+  });
+
+  // 03. infoProject > testBox
+  gsap.utils.toArray(".infoProject .textBox").forEach(function (textBox) {
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: textBox,
+        start: "50% 80%",
+        end: "100% 0%",
+        toggleClass: { targets: textBox, className: "active" },
+        scrub: 1,
+        // markers: true,
+      },
+    });
+  });
+
+  // 04. final > textBox
+  const final = gsap.timeline();
+  final
+    .fromTo(".final .t1", { autoAlpha: 1, duration: 1, y: -500, rotation: 0}, { autoAlpha: 1, duration: 1, y: '-50%', rotation: 360},"+=1")
+    .fromTo(".final .t2", { autoAlpha: 0, duration: 1, y: 50 }, { autoAlpha: 1, duration: 1, y:'-50%'}, "+=1")
+    .fromTo(".final .t3", { autoAlpha: 0, duration: 1, y: 50 }, { autoAlpha: 1, duration: 1, y: '-50%'}, "+=1")
+    .fromTo(".final .t4", { autoAlpha: 0, duration: 1, y: 50 }, { autoAlpha: 1, duration: 1, y: '-50%'}, "+=1")
+    .fromTo(".final .t5", { autoAlpha: 0, duration: 1, y: 50 }, { autoAlpha: 1, duration: 1, y: '-50%'}, "+=1")
+    .fromTo(".final .circle", {autoAlpha: 0, duration: 1},{autoAlpha: 1, duration: 5, scale : 10 , background: '#fff'}, "+=1")
+    .fromTo(".final .backSky", { autoAlpha: 0, duration: 1 , y: 50}, { autoAlpha: 1, duration: 1, y:0}, "+=1")
+
+  ScrollTrigger.create({
+    animation: final,
+    trigger: ".final",
+    start: "top top",
+    end: "+=7000",
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    // markers: true,
+  });
+
+  ScrollSmoother.create({
+    smooth: 1,
+    effects: true,
+  });
+});
+// intro_scrollTrigger [e]
+
 // star script [s]
 const sky = document.querySelector(".sky");
 const sky2 = document.querySelector(".sky2");
@@ -25,28 +115,28 @@ const makeStars = () => {
 // star script [e]
 
 // fade effect [s]
-function introFade() {
-    $('.backSky').fadeIn(1000);
-    setTimeout(titFade, 1000);
-}
+// function introFade() {
+//   $(".backSky").fadeIn(1000);
+//   setTimeout(titFade, 1000);
+// }
 
-function titFade() {
-  $(".backSky > .tit-box").fadeIn(1500).delay(2000);
-  $(".backSky > .tit-box").fadeOut(1000);
-  setTimeout(txtFade, 4500);
-}
+// function titFade() {
+//   $(".backSky > .tit-box").fadeIn(1500).delay(2000);
+//   $(".backSky > .tit-box").fadeOut(1000);
+//   setTimeout(txtFade, 4500);
+// }
 
-function txtFade() {
-  $(".backSky > .auto-box").fadeIn(1000);
-  setTimeout(mainMove, 3000);
-}
+// function txtFade() {
+//   $(".backSky > .auto-box").fadeIn(1000);
+//   setTimeout(mainMove, 3000);
+// }
 
-function mainMove() {
-    // git 전용
-    location.href ="../PYS_Portfolio/html/main.html";
-    // local 전용
-    // location.href ="/html/main.html" ;
-}
+// function mainMove() {
+  // git 전용
+  // location.href ="../PYS_Portfolio/html/main.html";
+  // local 전용
+  // location.href ="/html/main.html" ;
+// }
 // fade effect [e]
 
 window.onresize = () => {
@@ -54,6 +144,4 @@ window.onresize = () => {
 };
 window.onload = () => {
   makeStars();
-  introFade();
 };
-
